82ef8a186875cd6cbabaff9d607d82a35fa97126
Walter-Almeida-dev <Josewal004@gmail.com>
2024-06-27 16:48:16 -0300

    desafios

--- desafio.py
+++ desafio.py
@@ -1,3 +1,22 @@
-vagas = 0
+def calculo(m,n,lista_v):
+    vagas_d = []
+    while len(vagas_d) <= n:
+        for c in range(len(lista_v)):
+            print('')
+            
+    
 n = int(input('Vagas: '))
 m = int(input('Pessoas: '))
+lista_v = list()
+for i in range(1, n):
+    V_i = int(input('Posição: '))
+    
+
+
+
+
+
+
+#  for c in range(lista_v[c], -1,-1):
+#                 if c not in lista_v:
+#                     lista_v.append(c)
